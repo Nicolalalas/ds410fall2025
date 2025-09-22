8955d88d10d30a27b63bfb19cb04179514ef9486
Zhiyuan Chen <zjc5299@e5-cse-trantor01.cse.psu.edu>
2025-09-22 18:50:48 -0400

    fix

--- mrlab/q2/q2.py
+++ mrlab/q2/q2.py
@@ -1,30 +1,14 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
-import re
 
-WORD_RE = re.compile(r"[A-Za-z]+")
-VOWEL_RE = re.compile(r"[aeiou]", re.IGNORECASE)
+class Q2WordCount(MRJob):
+    def mapper(self, _, line):
+        for word in line.split():
+            yield word.lower(), 1
 
-def vowel_count(word):
-    return len(VOWEL_RE.findall(word))
+    def reducer(self, key, values):
+        yield key, sum(values)
 
-class Q2VowelFilteredWordCount(MRJob):
-    def steps(self):
-        return [MRStep(mapper=self.mapper_get_words,
-                       combiner=self.combiner_count_words,
-                       reducer=self.reducer_sum_counts)]
+if __name__ == '__main__':
+    Q2WordCount.run()
 
-    def mapper_get_words(self, _, line):
-        for word in WORD_RE.findall(line):
-            if vowel_count(word) >= 2:
-                yield word.lower(), 1
-
-    def combiner_count_words(self, word, counts):
-        yield word, sum(counts)
-
-    def reducer_sum_counts(self, word, counts):
-        yield word, sum(counts)
-
-if __name__ == "__main__":
-    Q2VowelFilteredWordCount.run()
 
--- mrlab/q3/q3.py
+++ mrlab/q3/q3.py
@@ -1,32 +1,17 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
-import re
 
-WORD_RE = re.compile(r"[A-Za-z]+")
-VOWEL_RE = re.compile(r"[aeiou]", re.IGNORECASE)
+class Q3WordCountFilter(MRJob):
+    def mapper(self, _, line):
+        for word in line.split():
+            yield word.lower(), 1
 
-def vowel_count(word):
-    return len(VOWEL_RE.findall(word))
+    def reducer(self, key, values):
+        total = sum(values)
+        vowels = sum(c in "aeiou" for c in key)
+        if total >= vowels:
+            yield key, total
 
-class Q3WordVowelFilteredCount(MRJob):
-    def steps(self):
-        return [MRStep(mapper=self.mapper_get_words,
-                       combiner=self.combiner_count_words,
-                       reducer=self.reducer_filter_sum)]
+if __name__ == '__main__':
+    Q3WordCountFilter.run()
 
-    def mapper_get_words(self, _, line):
-        for word in WORD_RE.findall(line):
-            if vowel_count(word) >= 2:
-                yield word.lower(), 1
-
-    def combiner_count_words(self, word, counts):
-        yield word, sum(counts)
-
-    def reducer_filter_sum(self, word, counts):
-        total = sum(counts)
-        if total >= vowel_count(word):
-            yield word, total
-
-if __name__ == "__main__":
-    Q3WordVowelFilteredCount.run()
 
