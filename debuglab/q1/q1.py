e315f12488957840481efc8857c444cedc6f0c55
Zhiyuan Chen <zjc5299@e5-cse-trantor01.cse.psu.edu>
2025-10-12 22:28:20 -0400

    fix

--- debuglab/q1/q1.py
+++ debuglab/q1/q1.py
@@ -1,28 +1,31 @@
 from mrjob.job import MRJob
 
-class CityStats(MRJob):
+class UninformativeClassName(MRJob):
+
     def mapper(self, _, line):
         fields = line.strip().split()
         if fields[0] == "name":
             return
-        state = fields[1]
+        city = fields[1]
+        thingy = fields[2]
         population = float(fields[3])
         zipcodes = fields[4].split(",")
-        yield state, (population, len(zipcodes))
+        yield thingy, (population, len(zipcodes))
 
     def reducer(self, state, values):
         total_pop = 0
-        total_cities = 0
-        zip_counts = []
-        for pop, zcount in values:
+        total_city = 0
+        max_zip = 0
+        for pop, z in values:
             total_pop += pop
-            total_cities += 1
-            zip_counts.append(zcount)
-        avg_pop = total_pop / total_cities
-        max_zip = max(zip_counts)
+            total_city += 1
+            if z > max_zip:
+                max_zip = z
+        avg_pop = total_pop / total_city
         yield state, (avg_pop, float(max_zip))
 
 if __name__ == '__main__':
-    CityStats.run()
+    UninformativeClassName.run()
+
 
 
