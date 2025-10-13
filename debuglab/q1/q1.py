30d9a83e42dc6be549e8b7fcac09daed10aebc5f
Zhiyuan Chen <zjc5299@e5-cse-trantor01.cse.psu.edu>
2025-10-12 21:22:17 -0400

    finish

--- debuglab/q1/q1.py
+++ debuglab/q1/q1.py
@@ -12,28 +12,19 @@ class CityStats(MRJob):
         if population >= 100 and zip_count >= 2:
             yield state, (population, zip_count)
 
-    def combiner(self, state, values):
-        total_pop = 0
-        total_zips = 0
-        count = 0
-        for p, z in values:
-            total_pop += p
-            total_zips += z
-            count += 1
-        yield state, (total_pop, total_zips, count)
-
     def reducer(self, state, values):
         total_pop = 0
         total_zips = 0
         total_cities = 0
-        for p, z, c in values:
+        for p, z in values:
             total_pop += p
             total_zips += z
-            total_cities += c
+            total_cities += 1
         avg_pop = total_pop / total_cities
         avg_zip = total_zips / total_cities
-        yield state, (round(avg_pop, 2), round(avg_zip, 2))
+        yield state, f"{round(avg_pop, 2)}\t{round(avg_zip, 2)}"
 
 if __name__ == "__main__":
     CityStats.run()
+
 
