9f0703ab2010865002c3e14d6be53c8013795980
Zhiyuan Chen <zjc5299@e5-cse-trantor01.cse.psu.edu>
2025-11-10 21:23:27 -0500

    fix

--- sparklab/q1/q1.scala
+++ sparklab/q1/q1.scala
@@ -1,7 +1,5 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.hadoop.conf.Configuration
 
 object Q1 {
   def main(args: Array[String]): Unit = {
@@ -15,26 +13,34 @@ object Q1 {
 
   def getRDD(sc: SparkContext): RDD[String] = sc.textFile("hdfs:///datasets/retailtab")
 
-  // For each customer, count distinct orders (InvoiceNo)
   def doRetail(sc: SparkContext, lines: RDD[String]): RDD[(String, Int)] = {
-    val body = lines
-      .filter(line => !line.startsWith("InvoiceNo\t"))
+    val pairs = lines
       .map { line =>
         val f = line.split("\t", -1)
-        val invoice  = if (f.length > 0) f(0) else ""
+        val invoice = if (f.length > 0) f(0) else ""
         val customer = if (f.length > 6) f(6) else ""
         (customer, invoice)
       }
-      .filter { case (_c, inv) => inv.nonEmpty }
+      .filter { case (_c, inv) => inv.nonEmpty && inv != "InvoiceNo" }
 
-    val distinctOrders = body.distinct()
-    distinctOrders.map { case (c, _inv) => (c, 1) }.reduceByKey((a, b) => a + b)
+    val distinctOrders = pairs.distinct()
+    distinctOrders.map { case (c, _) => (c, 1) }.reduceByKey((a, b) => a + b)
   }
 
+  def getTestRDD(sc: SparkContext): RDD[String] = sc.parallelize(Seq(
+    "InvoiceNo\tStockCode\tDescription\tQuantity\tInvoiceDate\tUnitPrice\tCustomerID\tCountry",
+    "10001\tA\tX\t1\tT\t1.0\t17850\tUK",
+    "10001\tB\tX\t1\tT\t1.0\t17850\tUK",
+    "10002\tC\tX\t1\tT\t1.0\t17850\tUK",
+    "10003\tD\tX\t1\tT\t1.0\t13047\tUK",
+    "10003\tE\tX\t1\tT\t1.0\t13047\tUK",
+    "10004\tF\tX\t1\tT\t1.0\t13047\tUK"
+  ))
+
+  def expectedOutput(sc: SparkContext): RDD[(String, Int)] =
+    sc.parallelize(Seq(("13047", 2), ("17850", 2)))
+
   def saveit(name: String, counts: RDD[(String, Int)]): Unit = {
-    val fs = FileSystem.get(new Configuration())
-    val p = new Path(name)
-    if (fs.exists(p)) fs.delete(p, true)
     counts.saveAsTextFile(name)
   }
 }
